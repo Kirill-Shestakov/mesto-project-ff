032bdc3c312e2d08613ee04a75f11f9955c2fadb
Kirill Shestakov <kirill.shestakov.03@gmail.com>
2024-01-08 01:33:34 +0700

    функциональность созданна

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const templateCard = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(name, link, callbackFunction) {
+    const card = templateCard.querySelector('.card').cloneNode(true);
+    card.querySelector('.card__title').textContent = name;
+    card.querySelector('.card__image').setAttribute('src', link);
+    const deleteButton = card.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', callbackFunction);
+    return card;
+};
+
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    const actionDeleteCard = evt.target.closest('.card')
+    actionDeleteCard.remove()
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item) {
+    placesList.append(createCard(item.name, item.link, deleteCard))
+});
+
+
