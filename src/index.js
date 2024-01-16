d56650106c2e69c4d78515ec5311f92dbcb0aea0
Kirill Shestakov <kirill.shestakov.03@gmail.com>
2024-01-16 22:10:41 +0700

    добавил оброботчик событий на кнопку +, которая открывает popupсоздания карточки места

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ const templateCard = document.querySelector("#card-template").content;
 const placesList = document.querySelector(".places__list");
 const popup = document.querySelector(".popup");
 const changeProfileButton = document.querySelector(".profile__edit-button");
+const addButtonProfile = document.querySelector('.profile__add-button')
 const popupEdit = document.querySelector(".popup_type_edit");
-const popupButtonClose = popupEdit.querySelector('.popup__close');
+const popupNewCard = document.querySelector('.popup_type_new-card')
+const closeNewCardButton = popupNewCard.querySelector('.popup__close');
+const closeProfileButton = popupEdit.querySelector('.popup__close');
 
 
 
@@ -16,15 +19,24 @@ initialCards.forEach(function (item) {
   placesList.append(createCard(item.name, item.link, deleteCard));
 });
 
-
-
 changeProfileButton.addEventListener('click', function() {
   openModal(popupEdit);
 
 });
 
-popupButtonClose.addEventListener('click', function() {
+closeProfileButton.addEventListener('click', function() {
   closeModal(popupEdit);
 });
 
-export {templateCard, placesList, popup, changeProfileButton, popupButtonClose}
+addButtonProfile.addEventListener('click', function () {
+  openModal(popupNewCard);
+});
+
+closeNewCardButton.addEventListener('click', function() {
+  closeModal(popupNewCard);
+});
+
+
+console.log(addButtonProfile)
+
+export {templateCard, placesList}
