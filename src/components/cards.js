51b8b22fcf698611451c6c0de62d7dcd76cc7f6e
Kirill Shestakov <kirill.shestakov.03@gmail.com>
2024-01-24 23:41:10 +0700

    доробал функции сайта

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,44 +1,55 @@
 /* ИМПОРТ МОДУЛЕЙ */
-import {templateCard, placesList, imageName, imageLink, popupNewCard} from '../index.js'
-import {openModal, closeModal} from './modal'
+import {
+  templateCard,
+  placesList,
+  imageName,
+  imageLink,
+  popupNewCard,
+  popupTypeImage,
+} from "../index.js";
+import { openModal, closeModal } from "./modal";
 
 /*Массив с карточками*/
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
 
 /* ФУНКЦИИ */
 /*Функция создания карточки*/
-function createCard(name, link, deleteCard) {
+function createCard(name, link, deleteCard, checkLike, openImgPopup) {
   const card = templateCard.querySelector(".card").cloneNode(true);
+  const deleteButton = card.querySelector(".card__delete-button");
+  const likeButton = card.querySelector(".card__like-button");
+  const image = card.querySelector(".card__image");
   card.querySelector(".card__title").textContent = name;
   card.querySelector(".card__image").setAttribute("src", link);
   card.querySelector(".card__image").setAttribute("alt", name);
-  const deleteButton = card.querySelector(".card__delete-button");
   deleteButton.addEventListener("click", deleteCard);
+  likeButton.addEventListener("click", checkLike);
+  image.addEventListener("click", openImgPopup);
   return card;
 }
 
@@ -48,12 +59,33 @@ function deleteCard(evt) {
   cardElement.remove();
 }
 
+/*Функция переключения лайка*/
+function checkLike(evt) {
+  evt.target.classList.toggle("card__like-button_is-active");
+}
+
+/*Функция открытия попап с изображением*/
+function openImgPopup(evt) {
+  openModal(popupTypeImage);
+  document.querySelector(".popup__image").src = evt.target.src;
+  document.querySelector(".popup__caption").textContent = evt.target.alt;
+}
+
 /*Функция добавления новой карточки*/
 function addNewCard(evt) {
   evt.preventDefault();
-  placesList.prepend(createCard(imageName.value, imageLink.value, deleteCard));
-  closeModal(popupNewCard)
-};
+  placesList.prepend(
+    createCard(imageName.value, imageLink.value, deleteCard, checkLike)
+  );
+  closeModal(popupNewCard);
+}
 
 /* ЭКСПОРТ МОДУЛЕЙ */
-export {initialCards, createCard, deleteCard, addNewCard};
+export {
+  initialCards,
+  createCard,
+  deleteCard,
+  checkLike,
+  openImgPopup,
+  addNewCard,
+};
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,13 +1,25 @@
+/* ИМПОРТ МОДУЛЕЙ */
+import { popupNewCard, popupTypeImage, popupEdit } from "../index.js";
+
 /* ФУНКЦИИ */
 /*Функция открытия попапа*/
 function openModal(elementDOM) {
-    elementDOM.classList.add('popup_is-opened')
+  elementDOM.classList.add("popup_is-opened");
 }
 
 /*Функция закрытия попапа*/
 function closeModal(elementDOM) {
-    elementDOM.classList.remove('popup_is-opened')
-};
+  elementDOM.classList.remove("popup_is-opened");
+}
+
+/*Функция закрытия попапов на кнопку 'Esc'*/
+function keydownEsc(evt) {
+  if (evt.key === "Escape") {
+    closeModal(popupEdit);
+    closeModal(popupNewCard);
+    closeModal(popupTypeImage);
+  }
+}
 
 /* ЭКСПОРТ МОДУЛЕЙ */
-export {openModal, closeModal};
+export { openModal, closeModal, keydownEsc };
