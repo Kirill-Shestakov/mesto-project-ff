036fdd902fd11996bf74e97be46e96b1cdf74642
Kirill Shestakov <kirill.shestakov.03@gmail.com>
2024-01-28 22:05:16 +0700

    доработал сайт

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,15 +1,18 @@
+const templateCard = document.querySelector("#card-template").content.querySelector(".card");
+
 /* ФУНКЦИИ */
 /*Функция создания карточки*/
 function createCard(name, link, deleteCard, checkLike, openImgPopup) {
-  const templateCard = document.querySelector("#card-template").content;
-  const card = templateCard.querySelector(".card").cloneNode(true);
+  const card = templateCard.cloneNode(true);
   const deleteButton = card.querySelector(".card__delete-button");
   const likeButton = card.querySelector(".card__like-button");
   const image = card.querySelector(".card__image");
   card.querySelector(".card__title").textContent = name;
   image.setAttribute("src", link);
   image.setAttribute("alt", name);
-  deleteButton.addEventListener("click", deleteCard);
+  deleteButton.addEventListener("click", () => {
+    deleteCard(card);
+  });
   likeButton.addEventListener("click", checkLike);
   image.addEventListener("click", () => {
       openImgPopup(name, link)
@@ -18,8 +21,7 @@ function createCard(name, link, deleteCard, checkLike, openImgPopup) {
 }
 
 /*Функция удаления карточки*/
-function deleteCard(evt) {
-  const cardElement = evt.target.closest(".card");
+function deleteCard(cardElement) {
   cardElement.remove();
 }
 
